ada02315827cc0ab658a919c208d7d577c424e7d
SHUBHAM BANSAL <500060901@stu.upes.ac.in>
2019-06-16 14:00:53 +0530

    Structure level easy

---
+++ Struct.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct  Student{
+int age;
+char first_name[6];
+char last_name[5];
+int standard;
+};
+int main() {
+    Student st;
+    cin >>st.age>>st.first_name>>st.last_name>>st.standard;
+    cout<<st.age<<" "<<st.first_name<<" "<<st.last_name<<" "<< st.standard;
+    return 0;
+}
+
+
