180c9db95334bef08d010ead9ac6b700163cf582
SHUBHAM BANSAL <500060901@stu.upes.ac.in>
2019-06-19 22:54:28 +0530

    Pointers level easy

---
+++ pointers.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+int c = *a;
+int d = *b;
+*a = c+d;
+if(c>=d)
+d=c-d;
+else
+d=d-c;
+*b=d;
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
+
+
