6aae322886637a981f97ace4515b7a25c1efba61
SHUBHAM BANSAL <500060901@stu.upes.ac.in>
2019-07-02 10:17:54 +0530

    Overloading operators

---
+++ Overload_Operators.cpp
@@ -0,0 +1,64 @@
+//Operator Overloading
+
+#include<iostream>
+
+using namespace std;
+
+class Complex
+{
+public:
+    int a,b;
+    void input(string s)
+    {
+        int v1=0;
+        int i=0;
+        while(s[i]!='+')
+        {
+            v1=v1*10+s[i]-'0';
+            i++;
+        }
+        while(s[i]==' ' || s[i]=='+'||s[i]=='i')
+        {
+            i++;
+        }
+        int v2=0;
+        while(i<s.length())
+        {
+            v2=v2*10+s[i]-'0';
+            i++;
+        }
+        a=v1;
+        b=v2;
+    }
+};
+
+Complex operator+(Complex C,Complex D)
+{
+	Complex temp;
+	temp.a= D.a+C.a;
+	temp.b= D.b+C.b;
+	return temp;
+}
+ostream & operator<< (ostream & out, const Complex & X) {
+    if (X.b < 0) {//Here we check also that imaginary part is zero or not
+        out << X.a << "-i" << -X.b;
+    } else if(X.b > 0) {
+        out << X.a << "+i" << X.b;
+    } else {
+        out << X.a;
+    }
+    return out;
+}
+
+int main()
+{
+    Complex x,y;
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+    x.input(s1);
+    y.input(s2);
+    Complex z=x+y;
+    cout<<z<<endl;
+}
+
