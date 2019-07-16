7904639969787c5aa4619026abf2845f481cc46d
SHUBHAM BANSAL <500060901@stu.upes.ac.in>
2019-07-16 18:00:22 +0530

    Version

--- DynamicArrays.cpp
+++ DynamicArrays.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
-#include<vector>
+#include<vector> //Imperative to include this header file inorder to non standard vectors.
 using namespace std;
 int main()
 {
 	int r,q;
 	cin>>r;
 	cin>>q;
-	vector<vector<int> > vec(r);//vector with r rows is created
+	vector<vector<int> > vec(r);//vector with r rows is created , (Columns in each row not yet defined)
     for(int i = 0; i<vec.size();i++)
     { 
         //"Enter the column size for the ith row"
         int c;
     	cin>>c;
-    	vec[i]= vector<int>(c);//for ith row, c columns will be created
+	//Most important learning here that how to create a vector at any index of vector
+    	vec[i]= vector<int>(c);//for ith row, c columns will be created(we can also declare rows and column together)
     	for(int j=0;j<c;j++)
     	{
     		int cv;//specific column_value
@@ -21,7 +22,7 @@ int main()
 		}
 	   
 	}
-	for(int i=0;i<q;++i)
+	for(int i=0;i<q;++i) 
 	{
 	int r,c;
 	cin>>r;
